51a0715a281ccf3c56e8858799d88be135268a60
ddeotti <deottidiego@gmail.com>
2024-06-25 22:10:52 -0300

    new committed Prep3 Prep4 and Prep5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int myGuess = -1;
+
+        // We could also use a do-while loop here...
+        while (myGuess != magicNumber)
+        {
+            Console.Write("What is your Guess? ");
+            myGuess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > myGuess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < myGuess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You Guessed it!");
+            }
+
+        }                    
     }
 }
