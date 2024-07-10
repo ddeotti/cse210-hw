ed02aa2424d0a3046af7db0ff4fcdd06b9d1c657
ddeotti <deottidiego@gmail.com>
2024-07-10 18:30:46 -0300

    Added a static "WaitKeyPress" method to the class program

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -3,7 +3,8 @@ using System.IO;
 using System.Collections.Generic;
 
 class Program
-{
+{   
+
     static void Main(string[] args)
     {
         bool running = true;
@@ -36,30 +37,25 @@ class Program
                     string inputText = Console.ReadLine();
                     string dateTimeStamp = DateTime.Now.ToString("F");
                     inputJournal.AddEntry(dateTimeStamp, actualPrompt, inputText);
-                    Console.WriteLine("Press any key to continue...");
-                    Console.ReadKey();
                     break;
                 case "2":
                     Console.WriteLine("Entries:");
                     inputJournal.DisplayAll();
-                    Console.WriteLine("Press any key to continue...");
-                    Console.ReadKey();
+                    WaitKeyPress();
                     break;
                 case "3":
                     Console.WriteLine("");
                     Console.Write("Write the filename to load: ");
                     fileName = Console.ReadLine();
                     inputJournal.LoadFromFile(fileName);
-                    Console.WriteLine("Press any key to continue...");
-                    Console.ReadKey();
+                    WaitKeyPress();
                     break;
                 case "4":
                     Console.WriteLine("");
                     Console.Write("Write the file name and its extension: ");
                     fileName = Console.ReadLine();
                     inputJournal.SaveToFile(fileName);
-                    Console.WriteLine("Press any key to continue...");
-                    Console.ReadKey();
+                    WaitKeyPress();
                     break;
                 case "5":
                     running = false;
@@ -67,12 +63,17 @@ class Program
                     break;
                 default:
                     Console.WriteLine("Invalid choice, please try again.");
-                    Console.WriteLine("Press any key to continue...");
-                    Console.ReadKey();
+                    WaitKeyPress();
                     break;
             }
 
             Console.WriteLine();
         }
+    }
+    static void WaitKeyPress() 
+    {
+        Console.WriteLine("");
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
     }
 }
