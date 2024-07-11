58afe341847c7db71651f205e1ef00bd2ba7c9fe
ddeotti <deottidiego@gmail.com>
2024-07-10 22:17:00 -0300

    Week 03 Encapsulation

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Console.Clear(); 
+        Console.WriteLine("constructor: Fraction");
+        Fraction fracao1 = new Fraction();
+        Console.WriteLine(fracao1.GetFractionString());
+        Console.WriteLine(fracao1.GetDecimalValue());
+
+
+        Console.WriteLine("");
+        Console.WriteLine("constructor: Fraction(int wholenumber)");
+        Fraction fracao2 = new Fraction(5);
+        Console.WriteLine(fracao2.GetFractionString());
+        Console.WriteLine(fracao2.GetDecimalValue());
+
+
+        Console.WriteLine("");
+        Console.WriteLine("constructor: Fraction(int top, int bottom)");
+        Fraction fracao3= new Fraction(3,4);
+        Console.WriteLine(fracao3.GetFractionString());
+        Console.WriteLine(fracao3.GetDecimalValue());
+    
+
+        Console.WriteLine("");
+        Console.WriteLine("constructor: Fraction(int top, int bottom) with 1/3 division");
+        Fraction fracao4 = new Fraction(1,3);
+        Console.WriteLine(fracao4.GetFractionString());
+        Console.WriteLine(fracao4.GetDecimalValue());
     }
 }
